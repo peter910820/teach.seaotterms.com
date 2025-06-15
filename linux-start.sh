6ece16bb3db9fa65f5366d6634d360ae36840095
peter910820 <peter910820@gmail.com>
2025-06-15 13:34:02 +0800

    chore: add linux-start batch

---
+++ linux-start.sh
@@ -0,0 +1,6 @@
+# yarn build
+
+export NITRO_PORT=3004
+export NITRO_HOST=127.0.0.1
+
+node .output/server/index.mjs
--- windwos-start.bat
+++ windwos-start.bat
@@ -2,7 +2,7 @@
 
 call yarn build
 
-set NITRO_PORT=3005
+set NITRO_PORT=3004
 set NITRO_HOST=127.0.0.1
 
 node .output/server/index.mjs
